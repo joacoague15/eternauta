919d1da6fe06c34e507b795fe9bd68e13269a188
joacoague15 <joacoague15@gmail.com>
2023-05-15 16:41:04 +0100

    Added new sounds

--- script.js
+++ script.js
@@ -19,8 +19,58 @@ const speak = () => {
     document.getElementById("eternauta-audio").play();
 }
 
-const playSnow = () => {
+const playSounds = () => {
     document.getElementById("modal").style.display = "none";
+
     document.getElementById("snow-audio").play();
+
+    respiracionAudio.volume = 0.2;
+    document.getElementById("respiracion").play();
+
+    document.getElementById("radio").volume = 0.1;
+    document.getElementById("radio").play();
+
     speak();
 }
+
+const eternautaAudio = document.getElementById("eternauta-audio");
+const respiracionAudio = document.getElementById("respiracion");
+
+eternautaAudio.addEventListener("playing", () => {
+    respiracionAudio.pause();
+});
+
+eternautaAudio.addEventListener("pause", () => {
+    respiracionAudio.play();
+});
+
+const playRifleShotSound = () => {
+    const rifleAudio = document.getElementById("disparo-rifle");
+
+    rifleAudio.volume = 0.1;
+    rifleAudio.play();
+};
+
+// Set an interval to play the sound every 8 seconds
+setInterval(playRifleShotSound, 12000);
+
+
+const favalliSosUnGenio = () => {
+    const favalli = document.getElementById("favalli-sos-un-genio");
+
+    favalli.volume = 0.1;
+    favalli.play();
+};
+
+// Set an interval to play the sound every 8 seconds
+setInterval(favalliSosUnGenio, 15000);
+
+const pabloNoVaAFuncionar = () => {
+    const pablo = document.getElementById("pablo-no-va-a-funcionar");
+
+    pablo.volume = 0.1;
+    pablo.play();
+};
+
+// Set an interval to play the sound every 8 seconds
+setInterval(pabloNoVaAFuncionar, 20000);
